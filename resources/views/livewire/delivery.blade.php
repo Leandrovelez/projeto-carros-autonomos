cf11a0295a2d18ee3e1ca4a6b9b8e967b062c091
Leandro Velez <leandrovelez55@gmail.com>
2024-11-17 21:55:57 -0300

    layout: corrigido o botão de visualizar a entrega

--- resources/views/livewire/delivery.blade.php
+++ resources/views/livewire/delivery.blade.php
@@ -4,4 +4,5 @@
     <x-mary-card class="dark:bg-slate-900">
         <livewire:delivery-list/>
     </x-mary-card>
+    <x-mary-icon hidden name="o-pencil-square" class=" hidden size-6 bg-amber-500 dark:bg-amber-800 hover:bg-amber-700 text-white dark:text-black" title="Excluir" />
 </div>
