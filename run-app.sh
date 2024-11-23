4d76af12de38cf1c98ffaf40a81482387bad5817
Leandro Velez <leandrovelez55@gmail.com>
2024-11-23 19:15:36 -0300

    backend: adicionado o seed no run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -2,3 +2,4 @@
 # Make sure this file has executable permissions, run `chmod +x run-app.sh`
 # Run migrations, process the Nginx configuration template and start Nginx
 php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
+php artisan db:seed --force
